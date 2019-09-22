9d1812be8011f940c4da0b92ada5b2ead8bcd7a2
Spencer Wallace <spencer.wallace@outlook.com>
2019-09-21 23:22:28 -0600

    Get movie information

--- scrape.js
+++ scrape.js
@@ -6,6 +6,22 @@ const scrape = () => {
   const $ = Cheerio.load(fs.readFileSync('video-page.html', 'utf8'))
   const movies = $('.movie-cover')
 
+  movieInfo = []
+  movies.each((i, movie) => {
+    movieInfo.push({
+      title:  $(movie).attr('data-item-title'),
+      kind: $(movie).attr('data-item-kind'),
+      id: $(movie).attr('data-item-id'),
+      duration: $(movie).attr('data-item-duration'),
+      series: $(movies).attr('data-item-series'),
+      description: $(movie).attr('data-item-description'),
+      rating: $(movie).attr('data-item-content-rating'),
+      href: $(movie).attr('data-item-href'),
+    })
+  })
+
+  
+  
 
 }
 
