4df17186fdca1729375237d0817b1b6d2dd70313
Spencer Wallace <spencer.wallace@outlook.com>
2019-09-23 22:38:25 -0600

    Can add movies and shows to airtable

--- scrape.js
+++ scrape.js
@@ -1,5 +1,7 @@
 const Cheerio = require('cheerio')
 const fs = require('fs')
+const Airtable = require('airtable')
+const base = new Airtable({apiKey: 'key4VeTEjoa318V75'}).base('appjf3A4Pbisud2Mn');
 
 const scrape = () => {
 
@@ -20,9 +22,53 @@ const scrape = () => {
     })
   })
 
-  
-  
+  movieInfo.forEach((movie) => {
+    store(movie)
+  })
+}
 
+const store = (movie) => {
+
+  base('movies').select({
+    view: "Grid view",
+    filterByFormula: `({id} = ${movie.id})`
+  }).firstPage((err, records) => {
+    if (err) { console.error(err); return; }
+    if (records.length) {
+      console.log('found a record')
+      update(records[0].id, movie)   
+      return
+    }
+      create(movie)      
+    })
+}
+
+const create = (movie) => {
+
+  base('movies').create([{fields: movie}], (err, records) => {
+    if (err) {
+      console.error(err); return;
+    }
+    records.forEach((record) => {
+      console.log(record.getId())
+    })
+  })
+}
+
+const update = (id, movie) => {
+  base('movies').update([
+    {
+      id: id,
+      fields: movie,
+    }
+  ], (err, records) => {
+    if (err) {
+      console.error(err); return;
+    }
+    records.forEach((record) => {
+      console.log(record.get('id'))
+    })
+  })
 }
 
 scrape()
